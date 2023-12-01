64e084740f831436b0a189bdb96611ee1fe3e470
nathalie <nathalie.metreau@hotmail.fr>
2023-12-01 15:45:19 +0100

    correct db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,13 +37,15 @@ class DBStorage:
         from models.city import City
         from models.user import User
         from models.place import Place
+        from models.review import Review
 
 
         tables = {
             'states': State,
             'cities': City,
             'users': User,
-            'places': Place
+            'places': Place,
+            'reviews': Review
 
         }
         objects = {}
