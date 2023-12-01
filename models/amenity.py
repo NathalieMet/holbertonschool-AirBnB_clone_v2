8b69c1458ab780e875c9244fe1dc056faf91ac5a
GTDeschamps <g.deschamps@hotmail.fr>
2023-12-01 18:17:42 +0100

    update amenity (propriety relationship  in comment )

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String,
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
@@ -11,5 +11,5 @@ class Amenity(BaseModel):
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
-    places = relationship("Place", secondary=place_amenity,
-                          back_populates="amenities")
+    """places = relationship("Place", secondary=place_amenity,
+                          back_populates="amenities")"""
