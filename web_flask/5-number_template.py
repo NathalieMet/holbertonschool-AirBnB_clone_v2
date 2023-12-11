52069b439e2f4e2096e1a5305896dbc2b91610eb
nathalie <nathalie.metreau@hotmail.fr>
2023-12-11 14:15:22 +0100

    push until task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -26,6 +26,10 @@ def diplay_python_and_text(text="is cool"):
 @app.route("/number/<int:n>", strict_slashes=False)
 def is_number(n):
     return f'{n} is a number'
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_HTML_page(n):
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     # Run the application on 0.0.0.0, port 5000
