a446d891e2d1fea0ebaf1aac7c222d6fcf54bd9b
nathalie <nathalie.metreau@hotmail.fr>
2023-12-13 14:04:31 +0100

    correct pycode on state for t7

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.city import City
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class State(BaseModel, Base):
     """ La classe State """
     __tablename__ = 'states'
@@ -13,13 +14,18 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+        cities = relationship(
+            'City',
+            backref='state',
+            cascade='all, delete-orphan')
     else:
         @property
         def cities(self):
             from models import storage
             return [city for city in storage.all(City).values()
                     if city.state_id == self.id]
+
+
 """class State(BaseModel, Base):
 
     __tablename__ = 'states'
