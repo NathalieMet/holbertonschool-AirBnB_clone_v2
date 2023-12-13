c884c72ab5b07316d61a0c536f1e9ba0924e1fb6
nathalie <nathalie.metreau@hotmail.fr>
2023-12-13 14:31:27 +0100

    correct t8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""  display a HTML page: (inside the tag BODY)
+"""
 from models import storage
 from flask import Flask, render_template
 from markupsafe import escape
@@ -10,6 +12,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """ display a HTML page: (inside the tag BODY) """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
@@ -17,9 +20,10 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """ remove the current SQLAlchemy Session """
     storage.close()
 
 
 if __name__ == '__main__':
-    # Run the application on 0.0.0.0, port 5000
+    """ Run the application on 0.0.0.0, port 5000 """
     app.run(host='0.0.0.0', port=5000)
